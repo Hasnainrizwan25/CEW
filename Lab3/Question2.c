6e68bb5e458e113b25448f1399bc3f06a69e6607
Hasnainrizwan25 <hasnainrizwan58@gmail.com>
2024-01-13 04:25:40 -0800

    push

---
+++ Lab3/Question2.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char *arr, int n) {
+    for (int i = n - 1; i >= 0; i--) {
+        printf("%c", *arr);
+        arr++;
+    }
+    printf("\n");
+}
+
+int main() {
+    char string[] = "Hasnain";
+
+    int n = strlen(string);
+
+    reverseString(string, n);
+
+    return 0;
+}
